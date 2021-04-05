eb5227870e15c63ad22c37e10f0b0c8f0525e0a7
ricozhuthegreat <ricozhuthegreat@hotmail.com>
2021-04-05 10:12:18 -0400

    Added Testing CHessboard

--- completeley-new-game/src/App.tsx
+++ completeley-new-game/src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Chessboard from "chessboardjsx";
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex-center">
+      <h1>Random Chess</h1>
+      <Chessboard
+        width={400}
+        position="rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+      />
     </div>
   );
-}
+};
 
 export default App;
